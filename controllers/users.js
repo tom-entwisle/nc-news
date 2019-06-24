681a96091f217058eaf33b89638b080cd40cec67
Tom Entwisle <tom@AZEROTH.local>
2019-06-24 15:46:09 +0100

    now handles 404 errors correctly

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,12 @@ const { fetchUserDataByUserName } = require("../models/users");
 
 exports.sendUserData = (req, res, next) => {
   username = req.params.username;
+  // console.log(username);
   fetchUserDataByUserName(username)
     .then(user => {
+      // console.log(user.length);
       if (user.length === 0)
-        return Promise.reject({ status: 404, msg: "user not found" });
+        res.status(404).send({ status: 404, msg: "user not found" });
       else {
         user = user[0];
         res.status(200).send({ user });

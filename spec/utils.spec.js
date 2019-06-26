cd92320bef09ce46100a06f921ef748d56a65f89
Tom Entwisle <tom@AZEROTH.local>
2019-06-26 15:17:25 +0100

    references changed to reflect modified util functions

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -9,7 +9,7 @@ const {
   formatDate,
   createReferenceObject,
   formatComments
-} = require("../db/data/utils/dataManip");
+} = require("../db/data/utils/");
 
 describe("formatDate()", () => {
   it("changes the date in milliseconds into a properly formatted date", () => {
@@ -21,24 +21,6 @@ describe("formatDate()", () => {
     expect(actual[3].created_at).to.eql(expected);
     expected = new Date(input[5].created_at);
     expect(actual[5].created_at).to.eql(expected);
-  });
-});
-
-describe("createReferenceObject()", () => {
-  it("takes the comments data and the inserted articles data as arguments and creates an object using the belongs_to value from the comments as the keys and the article_id values from the articles data as the values", () => {
-    const input = commentsData;
-    const input2 = articlesData;
-    input2.forEach((article, index) => {
-      article.article_id = index + 1;
-    });
-    const actual = createReferenceObject(input, input2);
-    const expected = {
-      "They're not exactly dogs, are they?": 9,
-      "Living in the shadow of a great man": 1,
-      "UNCOVERED: catspiracy to bring down democracy": 5,
-      A: 6
-    };
-    expect(actual).to.eql(expected);
   });
 });
 
@@ -184,5 +166,40 @@ describe("formatComments()", () => {
       }
     ];
     expect(actual).to.eql(expected);
+  });
+});
+
+describe("createReferenceObject", () => {
+  it("creates a reference object using the articles data and DOES NOT MUTATE the source data", () => {
+    const input = [];
+    const output = {};
+    expect(createReferenceObject(input)).to.eql(output);
+  });
+  it("returns a reference object containing the 'title' and 'article_id' key-value pairs, when an array of a single object is passed in", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        article_id: 1
+      }
+    ];
+    const output = {
+      "Living in the shadow of a great man": 1
+    };
+    expect(createReferenceObject(input)).to.eql(output);
+  });
+  it("returns a reference object containing the 'title' and 'article_id' key-value pairs, when an array of multiple objects are passed in", () => {
+    const input = [
+      { title: "Living in the shadow of a great man", article_id: 1 },
+      { title: "Sony Vaio; or, The Laptop", article_id: 2 },
+      { title: "Eight pug gifs that remind me of mitch", article_id: 3 },
+      { title: "Student SUES Mitch!", article_id: 4 }
+    ];
+    const output = {
+      "Living in the shadow of a great man": 1,
+      "Sony Vaio; or, The Laptop": 2,
+      "Eight pug gifs that remind me of mitch": 3,
+      "Student SUES Mitch!": 4
+    };
+    expect(createReferenceObject(input)).to.eql(output);
   });
 });
